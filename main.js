6861d30a04c3b8b2cd2e70351dc4bbd6dec02881
ivan <developerjsua@gmail.com>
2023-09-13 18:05:23 +0300

    features / add enemies

--- main.js
+++ main.js
@@ -10,98 +10,81 @@ const mouse = new THREE.Vector2();
 let camera;
 let scene;
 let renderer;
-let fbxModels = [];
-let bullets = [];
+const fbxModels = [];
+const bullets = [];
+const enemies = [];
 
 init();
-
 
 loadAndAddTurret('./public/turret.fbx', new THREE.Vector3(1, -2, 2));
 loadAndAddTurret('./public/turret.fbx', new THREE.Vector3(3, -2, 2));
 
 animate();
+setInterval(enemy, 1000);
 
 function init() {
-  // Create the camera
   camera = new THREE.PerspectiveCamera(
     70,
     window.innerWidth / window.innerHeight,
     0.01,
-    10
+    1000
   );
   camera.position.z = 1;
 
-  // Create the scene
   scene = new THREE.Scene();
   scene.background = new THREE.Color(params.color);
 
-  // Create the renderer
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  // Hide the default cursor and add the custom crosshair
   document.body.style.cursor = 'none';
-
 
   camera.position.set(0, 1, -3);
   camera.lookAt(0, 1, 0);
 
-  // Add mouse move event listener
   renderer.domElement.addEventListener('mousemove', onMouseMove);
-  renderer.domElement.addEventListener('click', fire)
+  renderer.domElement.addEventListener('click', fire);
 }
 
 function loadAndAddTurret(modelPath, position) {
   const loader = new FBXLoader();
 
   loader.load(modelPath, fbx => {
-    // Clone the model for each turret instance
     const clonedFbx = fbx.clone();
 
-    fbxModels.push(clonedFbx)
-    
-    // Apply scaling and position to the cloned model
+    fbxModels.push(clonedFbx);
+
     clonedFbx.scale.set(0.005, 0.005, 0.005);
     clonedFbx.position.copy(position);
-    
-    // Add the cloned turret model to the scene
+
     scene.add(clonedFbx);
   });
 }
 
-// function fire(event) {
-//   const bullet = new THREE.Mesh(
-//     new THREE.SphereGeometry(0.05, 16, 16), // adjust size
-//     new THREE.MeshBasicMaterial({ color: '#ff0000' })
-//   )
-//   let mouseDirection;
-//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+function enemy() {
+  const spawnX = 20;
+  const spawnY = Math.random() * 7 - 0;
+  const enemyZ = 20;
 
-//   fbxModels.forEach(item => {
-//     bullet.position.copy(item.position.clone())
+  const enemy = new THREE.Mesh(
+    new THREE.SphereGeometry(0.1, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  );
 
-//     mouseDirection = new THREE.Vector3(
-//       -mouse.x,
-//       mouse.y,
-//       item.position.z
-//     );
-//   })
-//   mouseDirection.normalize()
+  enemy.position.set(spawnX, spawnY, enemyZ);
 
-//   const bulletSpeed = 0.02
+  const enemySpeed = 0.04;
+  enemy.velocity = new THREE.Vector3(-enemySpeed, 0, 0);
 
-//   bullet.velocity = mouseDirection.multiplyScalar(bulletSpeed)
-
-//   bullets.push(bullet)
-//   scene.add(bullet)
-// }
+  enemies.push(enemy);
+  scene.add(enemy);
+}
 
 function fire(event) {
   fbxModels.forEach(item => {
     const bullet = new THREE.Mesh(
-      new THREE.SphereGeometry(0.05, 16, 16), // adjust size
+      new THREE.SphereGeometry(0.1, 16, 16), // adjust size
       new THREE.MeshBasicMaterial({ color: '#ff0000' })
     );
 
@@ -110,13 +93,13 @@ function fire(event) {
       -(event.clientY / window.innerHeight) * 2 + 1,
       -item.position.z
     );
-    
+
     mouseDirection.unproject(camera);
     mouseDirection.sub(camera.position).normalize();
-    
-    const bulletSpeed = 0.02;
+
+    const bulletSpeed = 1;
     bullet.velocity = mouseDirection.multiplyScalar(bulletSpeed);
-    
+
     bullet.position.copy(item.position.clone());
     bullets.push(bullet);
     scene.add(bullet);
@@ -144,12 +127,30 @@ function onMouseMove(event) {
   });
 }
 
+function updateTurrets() {
+  fbxModels.forEach(turret => {
+    enemies.forEach(enemy => {
+      const direction = new THREE.Vector3()
+        .subVectors(enemy.position, turret.position)
+        .normalize();
+
+      turret.lookAt(turret.position.clone().add(direction));
+    });
+  });
+}
+
 function animate() {
   requestAnimationFrame(animate);
+
+  updateTurrets();
+
   renderer.render(scene, camera);
 
   bullets.forEach(item => {
-    item.position.add(item.velocity)
-  })
-}
+    item.position.add(item.velocity);
+  });
 
+  enemies.forEach(enemy => {
+    enemy.position.add(enemy.velocity);
+  });
+}
