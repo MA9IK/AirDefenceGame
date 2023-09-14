fe7c013c036d46801d34c2920b7ae29c2b9ba38b
Ivan <developerjsua@gmail.com>
2023-09-14 11:16:34 +0300

    fix, feature / some fixes, add some ui

--- main.js
+++ main.js
@@ -11,20 +11,33 @@ const mouse = new THREE.Vector2();
 let camera;
 let scene;
 let renderer;
-let autocontrol = false;
-let shootCooldown = 0;
+let autocontrol = true;
+let shootCooldown = 2;
 let prevEnemyAngle = 0;
+let maxTargetDistance = 20; 
+let distanceTurret;
+let isPageActive = true;
 const fbxModels = [];
 const bullets = [];
 const enemies = [];
 
 init();
 
-loadAndAddTurret('./public/turret.fbx', new THREE.Vector3(1, -2, 2));
 loadAndAddTurret('./public/turret.fbx', new THREE.Vector3(3, -2, 2));
+loadAndAddTurret('./public/turret.fbx', new THREE.Vector3(-3, -2, 2));
 
 animate();
-setInterval(enemy, 1000);
+
+setInterval(enemy, 3000)
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "hidden") {
+    isPageActive = false;
+  } else {
+    isPageActive = true;
+  }
+});
+
 ui();
 
 function init() {
@@ -32,7 +45,7 @@ function init() {
     70,
     window.innerWidth / window.innerHeight,
     0.01,
-    1000
+    100
   );
   camera.position.z = 1;
 
@@ -68,19 +81,20 @@ function loadAndAddTurret(modelPath, position) {
 }
 
 function enemy() {
+  if (!isPageActive) return
   const spawnX = 20;
-  const spawnY = Math.random() * 7 - 0;
+  const spawnY = Math.random() * 9 - (-1);
   const enemyZ = 20;
 
   const enemy = new THREE.Mesh(
     new THREE.SphereGeometry(0.1, 16, 16),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
   );
-  enemy.scale.set(1, 1, 1);
+  enemy.scale.set(3, 3, 3);
 
   enemy.position.set(spawnX, spawnY, enemyZ);
 
-  const enemySpeed = 0.01;
+  const enemySpeed = 0.1;
   enemy.velocity = new THREE.Vector3(-enemySpeed, 0, 0);
 
   enemies.push(enemy);
@@ -88,13 +102,13 @@ function enemy() {
 }
 
 function fire(event) {
+  if (!autocontrol) return
   fbxModels.forEach(item => {
     const bullet = new THREE.Mesh(
       new THREE.SphereGeometry(0.1, 16, 16),
       new THREE.MeshBasicMaterial({ color: '#ff0000' })
     );
 
-    // Задаємо позицію пулі перед туреллю
     const bulletPosition = new THREE.Vector3(0, 0, -1)
       .applyQuaternion(item.quaternion)
       .add(item.position);
@@ -109,7 +123,7 @@ function fire(event) {
     mouseDirection.unproject(camera);
     mouseDirection.sub(camera.position).normalize();
 
-    const bulletSpeed = 1;
+    const bulletSpeed = 2;
     bullet.velocity = mouseDirection.clone().multiplyScalar(bulletSpeed);
 
     bullets.push(bullet);
@@ -121,22 +135,31 @@ function ui() {
   const gui = new dat.GUI();
 
   const customButton = document.createElement('button');
-  customButton.textContent = 'Enable control';
+  customButton.textContent = 'Enable autocontrol'
   customButton.addEventListener('click', function () {
     autocontrol = !autocontrol;
+    customButton.textContent = autocontrol ? 'Enable autocontrol' : 'Disable autocontrol'
   });
 
-  // Додаємо кнопку до інтерфейсу користувача
   gui.domElement.appendChild(customButton);
 
-  // Кастомізуємо стиль кнопки за допомогою CSS
-  customButton.style.color = 'white';
-  customButton.style.backgroundColor = 'blue';
-  customButton.style.border = 'none';
-  customButton.style.padding = '10px';
-  customButton.style.cursor = 'pointer';
+  customButton.style = `
+    color: white;
+    background-color: blue;
+    border: none;
+    padding: 10px;
+    cursor: pointer
+  `
 
-  // Приховуємо кнопку "close controls" за допомогою CSS
+  distanceTurret = document.createElement('div')
+  distanceTurret.textContent = `Curret turret distance - ${maxTargetDistance}`
+
+  distanceTurret.style = `
+    color: black;
+  `
+
+  gui.domElement.appendChild(distanceTurret)
+
   const closeControlsButton = gui.domElement.querySelector('.close-button');
   if (closeControlsButton) {
     closeControlsButton.style.display = 'none';
@@ -144,39 +167,41 @@ function ui() {
 }
 
 function onMouseMove(event) {
-  if (!autocontrol) {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-    const crosshairX = `${event.clientX - crosshair.width / 2}px`;
-    const crosshairY = `${event.clientY - crosshair.height / 2}px`;
+  const crosshairX = `${event.clientX - crosshair.width / 2}px`;
+  const crosshairY = `${event.clientY - crosshair.height / 2}px`;
 
-    crosshair.style.left = crosshairX;
-    crosshair.style.top = crosshairY;
-  } else {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  crosshair.style.left = crosshairX;
+  crosshair.style.top = crosshairY;
 
-    const crosshairX = `${event.clientX - crosshair.width / 2}px`;
-    const crosshairY = `${event.clientY - crosshair.height / 2}px`;
-
-    crosshair.style.left = crosshairX;
-    crosshair.style.top = crosshairY;
-
+  if (autocontrol) {
     const mousePosition = new THREE.Vector3(mouse.x, mouse.y, -1);
     mousePosition.unproject(camera);
     const direction = mousePosition.sub(camera.position).normalize();
 
     fbxModels.forEach(item => {
-      // Calculate the rotation to look at the direction
-      item.lookAt(item.position.clone().add(direction));
+      const rotationSpeed = 0.1;
+      
+      const angleToMouse = Math.atan2(direction.x, direction.z);
+
+      let angleDiff = angleToMouse - item.rotation.y;
+
+      if (angleDiff > Math.PI) angleDiff -= Math.PI * 2;
+      if (angleDiff < -Math.PI) angleDiff += Math.PI * 2;
+
+      item.rotation.y += angleDiff * rotationSpeed;
     });
   }
 }
 
+
 function removeEnemy(enemy) {
   const index = enemies.indexOf(enemy);
   if (index !== -1) {
+    maxTargetDistance += 0.5
+    distanceTurret.textContent = `Current turret distance - ${maxTargetDistance}`;
     scene.remove(enemy);
     enemies.splice(index, 1);
   }
@@ -185,17 +210,17 @@ function removeEnemy(enemy) {
 function updateTurrets() {
   fbxModels.forEach(turret => {
     if (!autocontrol) {
-      enemies.forEach(enemy => {
+      const target = findTargetInRange(turret.position);
+      
+      if (target) {
         const direction = new THREE.Vector3()
-          .subVectors(enemy.position, turret.position)
+          .subVectors(target.position, turret.position)
           .normalize();
 
-        // Знаходимо поточний кут ворога
         const currentEnemyAngle = Math.atan2(direction.x, direction.z);
 
-        // Згладжуємо кут між попереднім і поточним кутом
         const smoothAngle =
-          prevEnemyAngle + 0.1 * (currentEnemyAngle - prevEnemyAngle);
+          prevEnemyAngle + 0.2 * (currentEnemyAngle - prevEnemyAngle);
 
         turret.lookAt(
           turret.position
@@ -206,10 +231,11 @@ function updateTurrets() {
         );
 
         prevEnemyAngle = smoothAngle;
-      });
+      }
     }
   });
 }
+
 
 function animate() {
   requestAnimationFrame(animate);
@@ -222,9 +248,11 @@ function animate() {
 
   if (!autocontrol && shootCooldown <= 0) {
     fbxModels.forEach(turret => {
-      enemies.forEach(enemy => {
+      const target = findTargetInRange(turret.position);
+
+      if (target) {
         const direction = new THREE.Vector3()
-          .subVectors(enemy.position, turret.position)
+          .subVectors(target.position, turret.position)
           .normalize();
 
         const bullet = new THREE.Mesh(
@@ -232,27 +260,25 @@ function animate() {
           new THREE.MeshBasicMaterial({ color: '#ff0000' })
         );
 
-        bullet.velocity = direction.clone().multiplyScalar(1);
+        bullet.velocity = direction.clone().multiplyScalar(2);
         bullet.position.copy(turret.position.clone());
         bullets.push(bullet);
         scene.add(bullet);
 
         shootCooldown = 1;
-      });
+      }
     });
   }
 
   bullets.forEach((bullet, bulletIndex) => {
     bullet.position.add(bullet.velocity);
 
-    // Перевіряємо зіткнення пулі з ворогами
     enemies.forEach((enemy, enemyIndex) => {
       const distance = bullet.position.distanceTo(enemy.position);
 
       if (distance >= 1) {
         return;
       }
-      // Пуля попала в ворога, видаляємо пулю та ворога
       scene.remove(bullet);
       bullets.splice(bulletIndex, 1);
       removeEnemy(enemy);
@@ -263,3 +289,18 @@ function animate() {
     enemy.position.add(enemy.velocity);
   });
 }
+
+function findTargetInRange(turretPosition) {
+  for (const enemy of enemies) {
+    const distance = enemy.position.distanceTo(turretPosition);
+    if (distance <= maxTargetDistance) {
+      return enemy;
+    }
+  }
+  return null;
+}
+
+
+
+
+
