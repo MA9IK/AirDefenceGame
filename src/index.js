99c9cbf6a09ec06b4442a10c1c4e4539a3eabee4
Ivan <developerjsua@gmail.com>
2023-09-27 09:01:02 +0300

    features / ui

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 import * as CANNON from 'cannon-es';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const params = {
   color: 'blue'
 };
@@ -16,13 +14,11 @@ let renderer;
 let isPageActive = true;
 let changeTurretType;
 let changeButtonType;
-const rocketFuel = new Map();
-
+let addRocketLaunch;
+let addBasicTurret;
 const bullets = [];
-const rockets = [];
 const groundMeshes = [];
 const turretRotationAngles = [];
-
 const BulletTypes = {
   STANDARD: 'Standard',
   HOMING: 'Homing'
@@ -37,6 +33,7 @@ let changeGravity;
 let resetChangesButton;
 
 let gravityValue = new CANNON.Vec3(0, -9.82, 0);
+
 
 const enemies = [];
 const fbxModels = [];
@@ -65,16 +62,13 @@ loadAndAddTurret(
   fbxModels
 );
 
-setInterval(
-  () => {
-    const spawnX = 45;
-    const spawnY = Math.random() * 12 - -5;
-    const enemyZ = 20;
-    const position = new CANNON.Vec3(spawnX, spawnY, enemyZ);
-    createEnemy(position, world, scene, enemies);
-  },
-  Math.random() * 3000 + 6000
-);
+setInterval(() => {
+  const spawnX = 45;
+  const spawnY = Math.random() * 12 - -5;
+  const enemyZ = 20;
+  const position = new CANNON.Vec3(spawnX, spawnY, enemyZ);
+  createEnemy(position, world, scene, enemies);
+}, Math.random() * 3000 + 6000);
 
 animate();
 
@@ -84,14 +78,10 @@ scene.add(gridHelper);
 const axesHelper = new THREE.AxesHelper(4);
 scene.add(axesHelper);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.enableDamping = true;
-// controls.target.y = 0.1;
-// controls.update();
-
 document.addEventListener('visibilitychange', () => {
   isPageActive = document.visibilityState === 'hidden' ? false : true;
 });
+
 
 function init() {
   camera = new THREE.PerspectiveCamera(
@@ -103,7 +93,7 @@ function init() {
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(params.color);
-
+  
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -118,7 +108,7 @@ function init() {
   });
 }
 
-function loadAndAddTurret(modelPath, position, type, scale, name) {
+function loadAndAddTurret(modelPath, position, type, scale) {
   const loader = new FBXLoader();
 
   loader.load(modelPath, fbx => {
@@ -191,7 +181,6 @@ function ground() {
       mass: 0,
       shape: groundShape
     });
-
     groundBody.position.set(0, -1.7, 0);
     groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
 
@@ -263,47 +252,10 @@ function createBullet(turret, initialVelocity) {
   return { body: bulletBody, mesh: bulletMesh };
 }
 
-function createRocket(turret, initialVelocity) {
-  const fuel = 20; // liters
-  let mass = 50;
-  if (fuel) mass += fuel;
-
-  const rocketBody = new CANNON.Body({
-    mass,
-    shape: new CANNON.Sphere(0.05)
-  });
-
-  rocketFuel.set(rocketBody, fuel);
-
-  const rocketMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(0.05),
-    new THREE.MeshBasicMaterial({ color: 'green' })
-  );
-
-  const cannonPosition = new CANNON.Vec3(
-    turret.position.x,
-    turret.position.y,
-    turret.position.z
-  );
-
-  rocketBody.position.copy(cannonPosition);
-
-  rocketMesh.position.copy(turret.position);
-  rocketMesh.quaternion.copy(turret.quaternion);
-
-  rocketBody.velocity.set(
-    initialVelocity.x,
-    initialVelocity.y,
-    initialVelocity.z
-  );
-
-  world.addBody(rocketBody);
-  return { body: rocketBody, mesh: rocketMesh };
-}
-
 function fire(event) {
+  if (currentBulletType !== BulletTypes.STANDARD) return;
+
   const turret = fbxModels[0];
-  const secondTurret = fbxModels[1];
   const muzzleVelocity = 60;
 
   const mouseDirection = new THREE.Vector3(
@@ -323,15 +275,9 @@ function fire(event) {
 
   const initialVelocity = direction.clone().multiplyScalar(muzzleVelocity);
 
-  if (currentBulletType == BulletTypes.STANDARD) {
-    const bullet = createBullet(turret, initialVelocity);
-    bullets.push({ basic: bullet });
-    scene.add(bullet.mesh);
-  } else {
-    const rocket = createRocket(secondTurret, initialVelocity);
-    rockets.push(rocket);
-    scene.add(rocket.mesh);
-  }
+  const bullet = createBullet(turret, initialVelocity, world, scene);
+  bullets.push({ basic: bullet });
+  scene.add(bullet.mesh);
 }
 
 function toggleBulletType() {
@@ -340,7 +286,7 @@ function toggleBulletType() {
       ? BulletTypes.HOMING
       : BulletTypes.STANDARD;
 
-  changeTurretType.textContent = `Turret type - ${currentBulletType}`;
+  changeButtonType.textContent = `Turret type - ${currentBulletType}`;
   currentBullet = currentBulletType;
   changeButtonType.textContent = `Now type bullets - ${currentBulletType}`;
 }
@@ -348,30 +294,46 @@ function toggleBulletType() {
 function ui() {
   const gui = new dat.GUI();
 
-  // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ UI пїЅпїЅ ID
   changeTurretType = document.querySelector('#change-turret-button');
   changeButtonType = document.querySelector('#change-bullet-type-button');
-  changeGravity = document.querySelector('#gravity');
-  resetChangesButton = document.querySelector('#reset-changes');
+  changeGravity = document.querySelector('#gravity')
+  resetChangesButton = document.querySelector('#reset-changes')
+  addRocketLaunch = document.querySelector('#add-rocket-launch')
+  addBasicTurret = document.querySelector('#add-basic-turret')
 
+  changeButtonType.innerText = `Bullet type - ${currentBulletType}`;
   changeTurretType.innerText = `Turret type - ${currentBulletType}`;
-  changeButtonType.innerText = `Turret type - ${currentBulletType}`;
 
-  changeGravity.addEventListener('input', event => {
-    console.log(event.target.value);
-    gravityValue = new CANNON.Vec3(0, +event.target.value, 0);
+  changeGravity.addEventListener('input', (event) => {
+    console.log(event.target.value)
+    gravityValue = new CANNON.Vec3(0, +event.target.value, 0)
     world.gravity.copy(gravityValue);
-  });
+  })
 
-  resetChangesButton.addEventListener('click', event => {
-    gravityValue = new CANNON.Vec3(0, -9.82, 0);
-    world.gravity.copy(gravityValue);
-    changeGravity.value = '';
-  });
+  resetChangesButton.addEventListener('click', (event) => {
+    gravityValue = new CANNON.Vec3(0, -9.82, 0)
+    world.gravity.copy(gravityValue)
+    changeGravity.value = ''
+  })
 
-  // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
   changeTurretType.addEventListener('click', toggleBulletType);
   changeButtonType.addEventListener('click', toggleBulletType);
+  addRocketLaunch.addEventListener('click', (e) => {
+    let vector = new THREE.Vector3();
+    vector.set((e.clientX / window.innerWidth) * 2 - 1, -(e.clientY / window.innerHeight) * 2 + 1,0);
+    vector.unproject(camera)
+    let randomX = Math.random() * 4 - -1
+
+    loadAndAddTurret(
+      'turret.fbx',
+      new THREE.Vector3(randomX, -1, 2),
+      // vector,
+      turretTypes.HOMING,
+      0.005,
+      turrets,
+      fbxModels
+    );
+  })
 
   renderer.domElement.addEventListener('click', event => {
     fire(
@@ -393,6 +355,7 @@ function ui() {
   }
 }
 
+
 window.addEventListener('resize', function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -400,54 +363,9 @@ window.addEventListener('resize', function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-function updateRockets() {
-  rockets.forEach(rocket => {
-    const rocketBody = rocket.body;
-    const rocketMesh = rocket.mesh;
-
-    // Знайдіть найближчу ціль
-    let closestTarget = null;
-    let closestDistance = Infinity;
-
-    enemies.forEach(enemy => {
-      const enemyBody = enemy.body;
-      const distance = rocketBody.position.distanceTo(enemyBody.position);
-
-      if (distance < closestDistance) {
-        closestDistance = distance;
-        closestTarget = enemyBody;
-      }
-    });
-
-    if (closestTarget) {
-      // Обчисліть напрямок до цілі
-      const directionToTarget = new CANNON.Vec3();
-      closestTarget.position.vsub(rocketBody.position, directionToTarget);
-      directionToTarget.normalize();
-
-      // Змініть напрямок ракети на напрямок до цілі
-      rocketBody.velocity.copy(directionToTarget);
-
-      // Додайте випадкове відхилення
-      const randomDeviation = new CANNON.Vec3(
-        directionToTarget.x,
-        directionToTarget.y,
-        directionToTarget.z
-      );
-      randomDeviation.normalize();
-      rocketBody.velocity.vadd(randomDeviation, rocketBody.velocity);
-
-      // Оновіть позицію ракети
-      rocketMesh.position.copy(rocketBody.position);
-      rocketMesh.quaternion.copy(rocketBody.quaternion);
-    }
-  });
-}
-
 function animate() {
   requestAnimationFrame(animate);
 
-  updateRockets();
   fbxModels.forEach((item, index) => {
     const turretInfo = turrets[index];
 
@@ -488,27 +406,6 @@ function animate() {
 
   groundMeshes.forEach(groundMesh => {
     groundMesh.position.copy(new THREE.Vector3(0, -1.55, 0));
-  });
-
-  rockets.forEach(rocket => {
-    const rocketBody = rocket.body;
-    const rocketMesh = rocket.mesh;
-
-    const fuel = rocketFuel.get(rocketBody);
-
-    if (fuel > 0) {
-      rocketFuel.set(rocketBody, fuel - 0.05);
-
-      console.log(fuel);
-
-      rocketMesh.position.copy(rocketBody.position);
-      rocketMesh.quaternion.copy(rocketBody.quaternion);
-    } else {
-      scene.remove(rocketMesh);
-      world.removeBody(rocketBody);
-      rockets.splice(rockets.indexOf(rocket), 1);
-      rocketFuel.delete(rocketBody);
-    }
   });
 
   for (let i = bullets.length - 1; i >= 0; i--) {
@@ -553,8 +450,8 @@ function animate() {
     enemyMesh.position.copy(enemyBody.position);
     enemyMesh.quaternion.copy(enemyBody.quaternion);
   });
-
+  
   world.fixedStep();
-
+  
   renderer.render(scene, camera);
 }
